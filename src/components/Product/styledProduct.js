891e81f865a758ac3a6892d7cb05b539a57ad264
Omar <omaar_5@hotmail.com>
2021-04-11 12:44:50 +0200

    product Component Part 2

--- src/components/Product/styledProduct.js
+++ src/components/Product/styledProduct.js
@@ -1,19 +1,23 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    margin-bottom: 60px;
+`;
 
 export const Image = styled.img`
-    height: 400px;
+    height: 650px;
 `;
 
 export const Title = styled.h3`
-    font-size: 40px;
+    font-size: 30px;
     text-transform: uppercase;
     font-family: 'Oswald', sans-serif;
+    margin: 0 0 10px 0;
 `;
 
 export const Subtitle = styled.p`
     font-family: 'Oswald', sans-serif;
-    font-size: 30px;
+    font-size: 22px;
+    margin: 10px 0 0 0;
 `;
 
